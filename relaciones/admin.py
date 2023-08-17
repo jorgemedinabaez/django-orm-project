0d3ea508a011bcfca039351d60a12c9203b6cc0c
jorgemedinabaez <jorgemedinabaez88@gmail.com>
2023-08-16 22:29:21 -0400

    Se agregaron ejemplos para onetoone, onetomany y manytomany

--- relaciones/admin.py
+++ relaciones/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import ModeloAuto,Fabricante,TipoCombustible,DirectorEjecutivo
 
 # Register your models here.
+
+# Otra forma de registar los models aquí, sería elaborando una lista para luego pasarla por el 'admin.site.register':
+my_models = [ModeloAuto,Fabricante,TipoCombustible,DirectorEjecutivo]
+admin.site.register(my_models)
+# admin.site.register(Fabricante)
+# admin.site.register(ModeloAuto)
+# admin.site.register(TipoCombustible)
+# admin.site.register(DirectorEjecutivo)
+
+
--- relaciones/models.py
+++ relaciones/models.py
@@ -1,3 +1,44 @@
 from django.db import models
 
 # Create your models here.
+
+class Fabricante(models.Model):
+    nombre = models.CharField(max_length=255,verbose_name='Nombre fabricante')
+    created = models.DateTimeField(auto_now_add=True,verbose_name='Fecha de creación')
+    updated = models.DateTimeField(auto_now=True,verbose_name='Fecha de actualización')
+
+    def __str__(self):
+        return self.nombre
+
+class DirectorEjecutivo(models.Model):
+    fabricante = models.OneToOneField('Fabricante',on_delete=models.CASCADE)
+    nombre = models.CharField(max_length=255,verbose_name='Director Ejecutivo')
+    created = models.DateTimeField(auto_now_add=True,verbose_name='Fecha de creación')
+    updated = models.DateTimeField(auto_now=True,verbose_name='Fecha de actualización')
+
+    def __str__(self):
+        return self.nombre
+
+COMBUSTIBLE_CHOICES = [
+    ('Gasolina','Gasolina'),
+    ('Gas','Gas'),
+    ('Diesel','Diesel'),
+    ('Biodiesel','Biodiesel'),
+]
+class TipoCombustible(models.Model):
+    nombre = models.CharField(max_length=255,choices=COMBUSTIBLE_CHOICES,verbose_name='Tipo Combustible')
+    created = models.DateTimeField(auto_now_add=True,verbose_name='Fecha de creación')
+    updated = models.DateTimeField(auto_now=True,verbose_name='Fecha de actualización')
+
+    def __str__(self):
+        return self.nombre
+    
+class ModeloAuto(models.Model):
+    nombre = models.CharField(max_length=255,verbose_name='Modelo')
+    fabricante = models.ForeignKey('Fabricante',on_delete=models.SET_NULL,blank=True,null=True)
+    tipo_combustible = models.ManyToManyField('TipoCombustible')
+    created = models.DateTimeField(auto_now_add=True,verbose_name='Fecha de creación')
+    updated = models.DateTimeField(auto_now=True,verbose_name='Fecha de actualización')
+
+    def __str__(self):
+        return self.nombre
