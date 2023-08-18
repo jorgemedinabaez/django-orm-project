baede7309b965539ee0e673bc62668184b71744d
jorgemedinabaez <jorgemedinabaez88@gmail.com>
2023-08-18 02:43:24 -0400

    Se aplicaronconsultas SQL a travès del ORM de Django.

--- relaciones/models.py
+++ relaciones/models.py
@@ -1,9 +1,19 @@
+import datetime
 from django.db import models
+
+annos_choices = []
+
+for r in range(1950,(datetime.datetime.now().year+1)):
+    annos_choices.append((r,r))
+
+def anno_actual():
+    return datetime.date.today().year
 
 # Create your models here.
 
 class Fabricante(models.Model):
     nombre = models.CharField(max_length=255,verbose_name='Nombre fabricante')
+    pais = models.CharField(max_length=255, default='Desconocido',verbose_name='Pais')
     created = models.DateTimeField(auto_now_add=True,verbose_name='Fecha de creación')
     updated = models.DateTimeField(auto_now=True,verbose_name='Fecha de actualización')
 
@@ -36,7 +46,10 @@ class TipoCombustible(models.Model):
 class ModeloAuto(models.Model):
     nombre = models.CharField(max_length=255,verbose_name='Modelo')
     fabricante = models.ForeignKey('Fabricante',on_delete=models.SET_NULL,blank=True,null=True)
+    f_fabricacion = models.IntegerField(choices=annos_choices,default=anno_actual,verbose_name='Fecha de fabricación')
+    costo_fabricacion = models.DecimalField(null=False,decimal_places=2,max_digits=10,verbose_name='Costo de fabricación')
     tipo_combustible = models.ManyToManyField('TipoCombustible')
+    precio_venta = models.DecimalField(null=False,decimal_places=2,max_digits=10,verbose_name='Precio de venta')
     created = models.DateTimeField(auto_now_add=True,verbose_name='Fecha de creación')
     updated = models.DateTimeField(auto_now=True,verbose_name='Fecha de actualización')
 
